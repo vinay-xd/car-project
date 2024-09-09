a122751cf5afb633f82b74c4c8c6dc2b1c615d62
Vinay Panwar <155057248+vinay-xd@users.noreply.github.com>
2024-09-09 22:30:52 +0530

    Update Detailpage.jsx

--- client/src/pages/Detailpage.jsx
+++ client/src/pages/Detailpage.jsx
@@ -70,7 +70,7 @@ function Detailpage() {
         if (token) {
             setContact({ ...contact, carname: singlecar.title })
             try {
-                const response = await axios.post('https://car-project-server-sigma.vercel.app/contact-dealer', contact, {
+                const response = await axios.post('https://car-project-server.onrender.com/contact-dealer', contact, {
                     headers: {
                         Authorization: `Bearer ${token}`
                     }
